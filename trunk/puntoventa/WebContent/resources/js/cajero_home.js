a1a0730150ddc531a003ebb43e13a4f4315368d4
yair642 <yair642@4021ef09-9b9c-4e9a-a419-15370b4c346b>
2011-10-24 08:23:58 +0000

    clic puntoventa

--- trunk/puntoventa/WebContent/resources/js/cajero_home.js
+++ trunk/puntoventa/WebContent/resources/js/cajero_home.js
@@ -1,18 +1,19 @@
 function abrir_punto_venta(){	
 	$("#contenedor-principal").html(" <center><img  width='100px' height='100px' alt='loading...' src='resources/images/cargando.gif'/></center>");
-	
-}
-function abrir_punto_venta1(){	
-	$("#contenido").html("<img src='resources/images/cargando.gif'/>");
 	$.ajax({
 		type : "POST",
 		url : "menuCajero.do?abrir_punto_venta",
 		data: "parametro=1",
 		success : function(data) {
-			$("#contenido").html(data);
+			$("#contenedor-principal").html(data);
 		},
 		error : function(XMLHttpRequest, textStatus, errorThrown) {
 			alert(XMLHttpRequest.status + " ERROR!");
 		}
-	});		  
+	});	
+	
+}
+function abrir_punto_venta1(){	
+	$("#contenido").html("<img src='resources/images/cargando.gif'/>");
+	  
 }
