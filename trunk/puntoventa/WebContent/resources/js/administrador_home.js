3955615100463ac4adf2a52d1afe17263954f9ea
yair642 <yair642@4021ef09-9b9c-4e9a-a419-15370b4c346b>
2011-10-24 18:53:17 +0000

    agregar departamento

---
+++ trunk/puntoventa/WebContent/resources/js/administrador_home.js
@@ -0,0 +1,15 @@
+function abrir_crear_departamento(){	
+	$("#contenedor-principal").html(" <center><img  width='100px' height='100px' alt='loading...' src='resources/images/cargando.gif'/></center>");
+	$.ajax({
+		type : "POST",
+		url : "menuAdministrador?agregar_departamento",
+		data: "parametro=1",
+		success : function(data) {
+			$("#contenedor-principal").html(data);
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+			alert(XMLHttpRequest.status + " ERROR!");
+		}
+	});	
+	
+}
