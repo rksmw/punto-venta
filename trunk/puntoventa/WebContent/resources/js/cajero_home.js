9a588cd6f709fb276550da5bd2252954f4c9074a
yair642 <yair642@4021ef09-9b9c-4e9a-a419-15370b4c346b>
2011-11-01 00:29:30 +0000

    ventas -lista de ventas

--- trunk/puntoventa/WebContent/resources/js/cajero_home.js
+++ trunk/puntoventa/WebContent/resources/js/cajero_home.js
@@ -17,3 +17,42 @@ function abrir_punto_venta1(){
 	$("#contenido").html("<img src='resources/images/cargando.gif'/>");
 	  
 }
+
+function buscar_para_agregar(){	
+
+	$.ajax({
+		type : "POST",
+		url : "caja?buscar_para_agregar",
+		data: $('form').serialize(),
+		success : function(data) {
+			$("#resultado").html(data);
+			$( "#dialog-form" ).dialog( "open" );			
+			
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+			alert(XMLHttpRequest.status + " ERROR!");
+		}
+	});		
+}
+
+
+function agregar_producto_lista(){	
+	
+	$.ajax({
+		type : "POST",
+		url : "caja?agregar_producto_lista",
+		data: $('form').serialize(),
+		success : function(data) {
+			$("#contenedor-principal").html("");
+			$("#contenedor-principal").html(data);
+			$('#ui-dialog').remove();
+			$('#dialog-form').remove();
+			$('#resultado').html("");
+			$('div').remove('.ui-dialog');
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown) {
+			alert(XMLHttpRequest.status + " ERROR!");
+		}
+	});		
+}
+
