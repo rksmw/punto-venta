986b6eb3cd523a90f842c966eb7aab4e10a1a909
yair642 <yair642@4021ef09-9b9c-4e9a-a419-15370b4c346b>
2011-11-14 06:17:43 +0000

    los productos mas vendidos

--- trunk/puntoventa/WebContent/resources/js/reportes.js
+++ trunk/puntoventa/WebContent/resources/js/reportes.js
@@ -10,10 +10,13 @@ function abrir_ingreso_productos(){
 			error : function(XMLHttpRequest, textStatus, errorThrown) {
 				alert(XMLHttpRequest.status + " ERROR!");
 			}
-		});		
-	
+		});			
 }
 
 function abrirnuevo(){
 	Winid=window.open('reportes/reporte1.jsp','Reporte','width=100%, height=100%');
+}
+
+function abrirmasvendidos(){	
+	Winid=window.open('reportes/masvendis.jsp','Reporte','width=100%, height=100%');			
 }
